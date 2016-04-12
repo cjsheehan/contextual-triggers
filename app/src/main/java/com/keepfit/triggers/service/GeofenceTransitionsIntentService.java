2ca120eda1320bb4380d99a44550cc6454faeb38
mcnedward <edwardmcn64@gmail.com>
2016-04-12 16:58:39 +0100

    Add Geofences to LocationTrigger

---
+++ app/src/main/java/com/keepfit/triggers/service/GeofenceTransitionsIntentService.java
@@ -0,0 +1,69 @@
+package com.keepfit.triggers.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+/**
+ * Created by Edward on 4/12/2016.
+ */
+public class GeofenceTransitionsIntentService extends IntentService {
+    private static final String TAG = "GeofenceTransitions";
+
+    public GeofenceTransitionsIntentService() {
+        super(TAG);
+    }
+
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage = getErrorString(geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMessage);
+            return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // Get the transition details as a String.
+//            String geofenceTransitionDetails = getGeofenceTransitionDetails(
+//                    this,
+//                    geofenceTransition,
+//                    triggeringGeofences
+//            );
+
+            // Send notification and log the transition details.
+//            sendNotification(geofenceTransitionDetails);
+        } else {
+            // Log the error.
+            Log.e(TAG, "GEOFENCE ERROR...");
+        }
+    }
+
+    public static String getErrorString(int errorCode) {
+        switch (errorCode) {
+            case GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE:
+                return "Geofence not available";
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES:
+                return "Too many geofences";
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS:
+                return "Too many pending intents";
+            default:
+                return "Unknown geofence error";
+        }
+    }
+}
--- app/src/main/java/com/keepfit/triggers/thread/LocationThread.java
+++ app/src/main/java/com/keepfit/triggers/thread/LocationThread.java
@@ -1,61 +1,94 @@
 package com.keepfit.triggers.thread;
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.keepfit.triggers.TriggerService;
 import com.keepfit.triggers.listener.PermissionListener;
-import com.keepfit.triggers.utils.Broadcast;
+import com.keepfit.triggers.service.GeofenceTransitionsIntentService;
+import com.keepfit.triggers.utils.enums.TriggerPreference;
 import com.keepfit.triggers.utils.enums.TriggerType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Edward on 4/8/2016.
  */
 public class LocationThread extends TriggerThread<Object> implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
 
     private static final String TAG = "LocationThread";
     private static final String TITLE = "Location";
+    private static final int GEOFENCE_RADIUS_IN_METERS = 1000;
+    private static final int GEOFENCE_EXPIRATION_IN_MILLISECONDS = 600000;
+    private static final int GEOFENCE_LOITERING_DELAY = 10000;
+    private static final String HOME_KEY = "home";
+    private static final String WORK_KEY = "work";
+    private static final String CUSTOM_KEY = "custom";
 
+    private PermissionListener listener;
+    private SharedPreferences prefs;
     private GoogleApiClient googleApiClient;
     private Location lastLocation;
+    private List<Geofence> geoFences;
+    private PendingIntent geofencePendingIntent;
     private boolean accessGranted, locationReceived;
-    private PermissionListener listener;
 
     public LocationThread(Context context, PermissionListener listener) {
         super(TITLE, TriggerType.LOCATION, false, context);
         this.listener = listener;
+        geoFences = new ArrayList<>();
     }
 
     @Override
     public void doRunAction() {
         if (accessGranted && !locationReceived) {
+
+
             locationReceived = true;
         }
     }
 
     @Override
     public void doStartAction() {
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
         createGoogleApiClient();
-        createGeoFences();
         googleApiClient.connect();
     }
 
     @Override
     public void doStopAction() {
+        List<String> requestIds = new ArrayList<>();
+        for (Geofence geofence : geoFences)
+            requestIds.add(geofence.getRequestId());
+
+        LocationServices.GeofencingApi.removeGeofences(googleApiClient, requestIds)
+            .setResultCallback(this);
+
+        geoFences = new ArrayList<>();
         googleApiClient.disconnect();
+    }
+
+    @Override
+    public void onResult(Status status) {
+        Log.d(TAG, "GEOFENCES WORKED!!! " + status);
     }
 
     private void createGoogleApiClient() {
@@ -66,10 +99,6 @@ public class LocationThread extends TriggerThread<Object> implements
                     .addApi(LocationServices.API)
                     .build();
         }
-    }
-
-    private void createGeoFences() {
-        
     }
 
     @Override
@@ -84,11 +113,65 @@ public class LocationThread extends TriggerThread<Object> implements
         }
         lastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 googleApiClient);
+
+        GeofencingRequest request = createGeoFences();
+        geofencePendingIntent = getGeofencePendingIntent();
+
+        LocationServices.GeofencingApi.addGeofences(
+                googleApiClient,
+                request,
+                geofencePendingIntent
+        ).setResultCallback(this);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
         Log.w(TAG, "Connection suspended... " + i);
+    }
+
+    private GeofencingRequest createGeoFences() {
+        String homeLat = prefs.getString(TriggerPreference.HOME_LATITUDE.title, "");
+        String homeLong = prefs.getString(TriggerPreference.HOME_LATITUDE.title, "");
+        String workLat = prefs.getString(TriggerPreference.WORK_LATITUDE.title, "");
+        String workLong = prefs.getString(TriggerPreference.WORK_LONGITUDE.title, "");
+        String customLat = prefs.getString(TriggerPreference.CUSTOM_LATITUDE.title, "");
+        String customLong = prefs.getString(TriggerPreference.CUSTOM_LONGITUDE.title, "");
+
+        geoFences.add(createGeoFence(homeLat, homeLong, HOME_KEY));
+        geoFences.add(createGeoFence(workLat, workLong, WORK_KEY));
+        geoFences.add(createGeoFence(customLat, customLong, CUSTOM_KEY));
+
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
+        builder.addGeofences(geoFences);
+        return builder.build();
+    }
+
+    private Geofence createGeoFence(String latitude, String longitude, String key) {
+        double lat, lon;
+
+        try {
+            lat = Double.parseDouble(latitude);
+            lon = Double.parseDouble(longitude);
+        } catch (NumberFormatException e) {
+            Log.w(TAG, String.format("The latitude[%s] or longitude[%s] was invalid.", latitude, longitude), e);
+            return null;
+        }
+
+        return new Geofence.Builder()
+                .setRequestId(key)
+                .setCircularRegion(
+                        lat,
+                        lon,
+                        GEOFENCE_RADIUS_IN_METERS
+                )
+                .setLoiteringDelay(GEOFENCE_LOITERING_DELAY)
+                .setExpirationDuration(GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+                .setTransitionTypes(
+                        Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_EXIT |
+                        Geofence.GEOFENCE_TRANSITION_DWELL)
+                .build();
     }
 
     protected void createLocationRequest() {
@@ -100,6 +183,22 @@ public class LocationThread extends TriggerThread<Object> implements
 
     public void locationPermissionGranted() {
         accessGranted = true;
+    }
+
+    /**
+     * Source: http://developer.android.com/training/location/geofencing.html
+     *
+     * @return
+     */
+    private PendingIntent getGeofencePendingIntent() {
+        if (geofencePendingIntent != null)
+            return geofencePendingIntent;
+
+        Intent intent = new Intent(context, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        return PendingIntent.getService(context, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
     }
 
     @Override
